2c0be05c218e78966a31097c1c53115b30736e8c
Gustavo <gcostacoelho2003@gmail.com>
2025-06-25 17:07:43 -0300

    feat: created user controllers

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from "tsyringe";
 import { Request, Response } from "express";
 import { HttpStatusCode } from "../models/enums/HttpStatusCode";
 import IUserService from "../services/user/interfaces/IUserService";
+import { BODY_REQUEST_INVALID } from "../models/utils/Constants";
 
 @injectable()
 export default class UserController {
@@ -13,16 +14,46 @@ export default class UserController {
     async GetUserPerId(req: Request, res: Response): Promise<void> {
         const { id } = req.params;
 
-        const user = await this._userService.GetPerId(id);
+        const response = await this._userService.GetPerId(id);
 
-        res.status(HttpStatusCode.OK).json(user);
+        res.status(HttpStatusCode.OK).json(response);
     }
 
     async GetUserPerEmail(req: Request, res: Response): Promise<void> {
         const { email } = req.params;
 
-        const user = await this._userService.GetPerMail(email);
+        const response = await this._userService.GetPerMail(email);
 
-        res.status(HttpStatusCode.OK).json(user);
+        res.status(HttpStatusCode.OK).json(response);
+    }
+
+    async CreateNewUser(req: Request, res: Response): Promise<void> {
+        const userData = req.body;
+
+        if (userData && Object.keys(userData).length > 0) {
+            const response = await this._userService.Create(userData);
+
+            res.status(HttpStatusCode.OK).json(response);
+        } else {
+            res.status(HttpStatusCode.BAD_REQUEST).json(BODY_REQUEST_INVALID);
+        }
+    }
+
+    async UpdateUser(req: Request, res: Response): Promise<void> {
+        const { id } = req.params;
+
+        const userData = req.body;
+
+        const response = await this._userService.Update(userData, id);
+
+        res.status(HttpStatusCode.OK).json(response);
+    }
+
+    async DeleteUser(req: Request, res: Response): Promise<void> {
+        const { id } = req.params;
+
+        await this._userService.Delete(id);
+
+        res.status(HttpStatusCode.NO_CONTENT).json();
     }
 }
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -11,4 +11,20 @@ router.get("/:id", (req: Request, res: Response) =>
     userController.GetUserPerId(req, res),
 );
 
+router.get("/:email", (req: Request, res: Response) =>
+    userController.GetUserPerEmail(req, res),
+);
+
+router.post("", (req: Request, res: Response) =>
+    userController.CreateNewUser(req, res),
+);
+
+router.put("/:id", (req: Request, res: Response) =>
+    userController.UpdateUser(req, res),
+);
+
+router.delete("/:id", (req: Request, res: Response) =>
+    userController.DeleteUser(req, res),
+);
+
 export default router;
